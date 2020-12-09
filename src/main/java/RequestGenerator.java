b5485d03832fde13fe93b5fbb46f845ab9c4ae91
nordje <nordje@student.chalmers.se>
2020-12-09 13:17:28 +0100

    Add multiple users
    Added multiple users to the request generator. It will now generate requests for multiple users and increment the userid. It will send request 1 for all users and then move on to sending request 2 for all users and so on. I removed the sleep timer since I misunderstood the requirement about the time interval.
    
    Resolves #3

--- src/main/java/RequestGenerator.java
+++ src/main/java/RequestGenerator.java
@@ -2,31 +2,31 @@ import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import java.util.UUID;
 
 /**
  * RequestGenerator is created to help stress test the entire distributed system with various loads.
  * Example/default scenario: 100 different users each submitting single booking requests for a single dental office
  * within a time-interval of ten seconds.
  *
- * Current setup: 3 booking requests for the same time slot from one user to the same dentist with  a time-interval
- * of ten seconds.
+ * Current setup: 3 booking requests for the same time slot from 3 users to the same dentist.
  */
 public class RequestGenerator {
     private final static int NUMBER_OF_REQUESTS_PER_USER = 3;
-
-    private final static long INTERVAL = 10000; // Milliseconds
 
     private final static String TOPIC = "BookingRequest";
 
     private final static String BROKER = "tcp://localhost:1883";
 
-    private final static String USER_ID = "request-generator";
+    private final static String CLIENT_ID = "request-generator";
+
+    private final static int DENTIST_ID = 1;
+
+    private final static int NUMBER_OF_USERS = 3;
 
     private final IMqttClient middleware;
 
     public RequestGenerator() throws MqttException {
-        middleware = new MqttClient(BROKER, USER_ID);
+        middleware = new MqttClient(BROKER, CLIENT_ID);
         middleware.connect();
     }
 
@@ -47,27 +47,17 @@ public class RequestGenerator {
     }
 
     /**
-     * Publishes requests n number of times to the broker.
+     * Publishes requests n number of times to the broker per NUMBER_OF_USERS.
+     * j represents userid. userid will start at 1 and increment from there.
+     * i represents requestid. requestid will start at 1 and increment from there.
      * @param number n times to publish request
      * @throws MqttException
      */
     private void publishRequest(int number) throws MqttException {
-        for (int i = 0; i < number; i++) {
-            sleep();
-            sendRequest(fakeBooking(i));
-            System.out.println("Sent request number " + (i+1));
-        }
-    }
-
-    /**
-     * Puts the thread to sleep for as long as the INTERVAL is set to. Small modifications, such as the parameters
-     * and the use of the INTERVAL variable, have been made to the example code.
-     */
-    private void sleep() {
-        try {
-            Thread.sleep(INTERVAL);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 1; i < number+1; i++) {
+            for (int j = 1; j < NUMBER_OF_USERS+1; j++) {
+                sendRequest(fakeBooking(j, i));
+            }
         }
     }
 
@@ -83,26 +73,23 @@ public class RequestGenerator {
     }
 
     /**
-     * Creates a fake booking based on the number of the request.
-     * TODO: Update userid, dentistid, time to be dynamic
-     * @param number request number
+     * Creates a fake booking.
+     * TODO: Update time to be dynamic & should userid = UUID.randomUUID() ?
+     * @param userid user id
+     * @param requestid request id/number of the request
      * @return Booking String/JSON
      */
-    private String fakeBooking(int number) {
-        int userid = 99999; // UUID.randomUUID() ?
-        int requestid = number+1;
-        int dentistid = 1;
+    private String fakeBooking(int userid, int requestid) {
         long issuance = System.currentTimeMillis();
         String time = "2020-12-14 14:30";
 
         // The print outs are just for testing purposes
-        System.out.println("Possible user id: " + UUID.randomUUID());
         System.out.println("{\n \"userid\": " + userid + ",\n\"requestid\": " + requestid +
-                ",\n\"dentistid\": " + dentistid + ",\n\"issuance\": " + issuance +
+                ",\n\"dentistid\": " + DENTIST_ID + ",\n\"issuance\": " + issuance +
                 ",\n\"time\": \"" + time + "\" \n}");
 
         return "{\n \"userid\": " + userid + ",\n\"requestid\": " + requestid +
-                ",\n\"dentistid\": " + dentistid + ",\n\"issuance\": " + issuance +
+                ",\n\"dentistid\": " + DENTIST_ID + ",\n\"issuance\": " + issuance +
                 ",\n\"time\": \"" + time + "\" \n}";
     }
 }
